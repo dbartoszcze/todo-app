62bcc4cabcbf414e6f238caab2df4d668e320d34
Dominik Bartoszcze <dbartoszcze2@gmail.com>
2020-12-17 12:36:03 +0100

    feat: Create add task functionality

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,11 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 import '../styles/AddTask.css'
-const AddTask = () => {
+const AddTask = ({ addTask }) => {
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [isImportant, setIsImportant] = useState(false);
+
+    const handleChange = (e) => {
+        switch (e.target.name) {
+            case 'title':
+                setTitle(e.target.value)
+                return;
+            case 'description':
+                setDescription(e.target.value)
+                return;
+            case 'isImportant':
+                setIsImportant(e.target.checked)
+                return;
+            default:
+                console.warn('Wrong input name!')
+                return;
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (title) {
+            addTask({title, description, isImportant});
+        }
+    }
+
     return (
         <div className='addTask'>
-            Add Task
+            <form onSubmit={handleSubmit}>
+
+                <label>
+                    Nazwa zadania:
+                <input
+                        type="text"
+                        value={title}
+                        onChange={handleChange}
+                        name="title"
+                        placeholder='Tytuł...'
+                        required
+                    />
+                </label>
+                <label>
+                    Opis:
+                <input
+                        type="text"
+                        value={description}
+                        onChange={handleChange}
+                        name="description"
+                        placeholder='Opis..'
+                    />
+                </label>
+
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={isImportant}
+                        onChange={handleChange}
+                        name="isImportant"
+                    />
+                    Czy ważne zadanie?
+
+                </label>
+
+                <button>Dodaj zadanie</button>
+            </form>
+
         </div>
     );
 }
