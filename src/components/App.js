915f33e0f1d6e2e113ecaf8b9625e9e612ee159e
Dominik Bartoszcze <dbartoszcze2@gmail.com>
2020-12-17 13:16:07 +0100

    feat: Use created components for adding and displaying tasks

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AddTask from './AddTask';
 import TaskList from './TaskList';
@@ -6,10 +6,39 @@ import TaskList from './TaskList';
 import '../styles/App.css'
 
 const App = () => {
+
+    const [tasks, setTasks] = useState([{
+        id: 123,
+        title: 'Task1',
+        description: 'Desc1',
+        isImportant: true
+    },
+    {
+        id: 124,
+        title: 'Task2',
+        description: 'Desc2',
+        isImportant: false
+    }]);
+
+    const handleAddTask = ({ title, description, isImportant }) => {
+        const task = {
+            title,
+            description,
+            isImportant,
+            id: Math.floor(Math.random() * 1234)
+        }
+
+        setTasks(prev => [...prev, task])
+    }
+
+    const handleRemoveTask = (id) => {
+        setTasks(prevTasks => prevTasks.filter(task => task.id !== id))
+    }
+
     return (
         <div className='app'>
-            <AddTask />
-            <TaskList />
+            <AddTask addTask={handleAddTask} />
+            <TaskList removeTask={handleRemoveTask} tasks={tasks} />
         </div>
     );
 }
