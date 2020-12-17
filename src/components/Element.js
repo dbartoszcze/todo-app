005f40732adc402df28e65c0e52660bc48575807
Dominik Bartoszcze <dbartoszcze2@gmail.com>
2020-12-17 13:09:24 +0100

    feat: Add styles for each added task

--- src/components/Element.js
+++ src/components/Element.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import '../styles/Element.css'
 const Element = ({ title, description, isImportant }) => {
     return (
-        <div>
-            <p>{title}</p>
+        <div className={isImportant ? 'element important' : 'element'}>
+            <h3 className='taskHeader'>{title}</h3>
+            <p className='taskDescription'>{description}</p>
+
+            <div className='actionBtns'>
+                <button>Zmień na ważny</button>
+                <button>Usuń</button>
+            </div>
         </div>
     );
 }
